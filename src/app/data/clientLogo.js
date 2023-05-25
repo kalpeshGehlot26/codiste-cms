624f250c2b44ca389dc0bfd45e7647c9d0c4a1a2
Codiste <kunal.s@codiste.com>
2023-05-25 17:58:44 +0530

    svg logo add

--- src/app/data/clientLogo.js
+++ src/app/data/clientLogo.js
@@ -1,60 +1,71 @@
 const clientLogos = [
     {
-        url: "assets/images/splide/apollo.jpg",
+        url: "assets/images/splide/Apollo.svg",
         alt: "Apollo",
+    },
+    {
+        url: "assets/images/splide/Bonfire.svg",
+        alt: "Bonfite"
+    },
+    {
+        url: "assets/images/splide/coinxPad.svg",
+        alt: "Coinxpad"
+    },
+    {
+        url: "assets/images/splide/counterTen.svg",
+        alt: "counterTen"
+    },
+    {
+        url: "assets/images/splide/Cypha.svg",
+        alt: "Cypha"
+    },
+    {
+        url: "assets/images/splide/DiveWallet.svg",
+        alt: "DiveWallet"
+    },
+    {
+        url: "assets/images/splide/FTWDAO.svg",
+        alt: "FtwDao"
+    },
+    {
+        url: "assets/images/splide/Holygrails.svg",
+        alt: "Holygrails"
+    },
+
+
+    {
+        url: "assets/images/splide/Ikaris.svg",
+        alt: "Ikaris"
+    },
+    {
+        url: "assets/images/splide/Medizen.svg",
+        alt: "Medizen"
+    },
+
+    {
+        url: "assets/images/splide/ml.svg",
+        alt: "ML"
+    },
+
+    {
+        url: "assets/images/splide/NearPro.svg",
+        alt: "Near Pro"
+    },
+
+
+
+    {
+        url: "assets/images/splide/mumble.jpg",
+        alt: "Mumble"
     },
     {
         url: "assets/images/splide/Bloqhodler.jpg",
         alt: "Bloqhodler"
     },
     {
-        url: "assets/images/splide/bonfite-1.jpg",
-        alt: "Bonfite"
-    },
-    {
-        url: "assets/images/splide/coinxPadLogo.svg",
-        alt: "Coinxpad"
-    },
-    {
-        url: "assets/images/splide/cypha.jpg",
-        alt: "Cypha"
-    },
-    {
-        url: "assets/images/splide/DiveWallet.png",
-        alt: "DiveWallet"
-    },
-    {
-        url: "assets/images/splide/ftwDao.jpg",
-        alt: "FtwDao"
-    },
-    {
         url: "assets/images/splide/galaxyCoin.jpg",
         alt: "Galaxy Coin"
     },
-    {
-        url: "assets/images/splide/holygrails.jpg",
-        alt: "Holygrails"
-    },
-    {
-        url: "assets/images/splide/ikaris.jpg",
-        alt: "Ikaris"
-    },
-    {
-        url: "assets/images/splide/medizen.jpg",
-        alt: "Medizen"
-    },
-    {
-        url: "assets/images/splide/ml.jpg",
-        alt: "ML"
-    },
-    {
-        url: "assets/images/splide/mumble.jpg",
-        alt: "Mumble"
-    },
-    {
-        url: "assets/images/splide/nearPro.jpg",
-        alt: "Near Pro"
-    }
 
 ];
 
