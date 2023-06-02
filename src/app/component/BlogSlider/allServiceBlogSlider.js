030dafc6bdcacf0c1487ecf22bbff7ef27655d7d
Codiste <kunal.s@codiste.com>
2023-06-02 19:31:48 +0530

    fix ui for blog

--- src/app/component/BlogSlider/allServiceBlogSlider.js
+++ src/app/component/BlogSlider/allServiceBlogSlider.js
@@ -6,24 +6,24 @@ import { getPostsByCategory } from "@/app/lib/lib";
 import Link from "next/link";
 
 const AllServiceBlogSlider = ({ limit, category }) => {
-    
+
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(false);
 
     const getBlogs = async () => {
-		setLoading(true);
-		getPostsByCategory(limit, category)
-			.then((data) => {
-				setPosts((prevState) => [...prevState, ...data.data]);
-			})
-			.catch((error) => console.error(error))
-			.finally(() => setLoading(false));
-	};
+        setLoading(true);
+        getPostsByCategory(limit, category)
+            .then((data) => {
+                setPosts((prevState) => [...prevState, ...data.data]);
+            })
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    };
 
-	useEffect(() => {
-		getBlogs();
-	}, [limit, category]);
-    
+    useEffect(() => {
+        getBlogs();
+    }, [limit, category]);
+
     console.log("posts", posts)
 
     return (
@@ -41,23 +41,21 @@ const AllServiceBlogSlider = ({ limit, category }) => {
                     speed: 2,
                 },
                 breakpoints: {
-                    1400: {
-                        perPage: 4,
-                    },
+
                     1000: {
                         perPage: 3,
                     },
                     800: {
                         perPage: 2,
                     },
-                    400: {
+                    500: {
                         perPage: 1,
                     }
                 }
             }}
             className="slide-pagination">
             <SplideTrack>
-                {posts.length ? 
+                {posts.length ?
                     (
                         posts?.map((post, index) => {
                             const link = post.link.split("/");
@@ -65,15 +63,15 @@ const AllServiceBlogSlider = ({ limit, category }) => {
                             return (
                                 <SplideSlide className="splide__slide p-2" key={index}>
                                     <div className="position-relative bloghight-set">
-                                        <img className="img-fluid rounded p-0" 
+                                        <img className="img-fluid rounded p-0"
                                             alt={post.title.rendered}
-                                            src={post._embedded["wp:featuredmedia"]?.["0"].code ? image : post._embedded["wp:featuredmedia"]?.["0"]?.source_url} 
+                                            src={post._embedded["wp:featuredmedia"]?.["0"].code ? image : post._embedded["wp:featuredmedia"]?.["0"]?.source_url}
                                         />
                                         <div className="blogboxbg">
                                             <div className="blogfont-pos">
                                                 <p>{post._embedded["wp:term"][0][0]?.name}{" "}Development Service</p>
                                                 <h4>{post.title.rendered}</h4>
-                                                <Link href={link[3]} className="blogfont-btn">Read More</Link>
+                                                <Link href={link[3]} className="blogfont-btn mt-2">Read More</Link>
                                             </div>
                                         </div>
                                     </div>
@@ -81,9 +79,9 @@ const AllServiceBlogSlider = ({ limit, category }) => {
                             );
                         })
                     ) : (
-                        <div style={{width: "100%", display: "flex", justifyContent: "center"}}>
-						    <div className="spin"></div>
-					    </div>
+                        <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                            <div className="spin"></div>
+                        </div>
                     )
                 }
             </SplideTrack>
