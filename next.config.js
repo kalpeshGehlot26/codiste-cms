c74c564f1c6dc71ec035e779ee2dd73fecbcaa3c
sonalipanchal <sonali.p@codiste.com>
2023-08-04 11:10:49 +0530

    Config changes

--- next.config.js
+++ next.config.js
@@ -30,47 +30,7 @@ module.exports = {
             value: 'public, max-age=31536000, immutable', // Set caching for assets to one year (31536000 seconds)
           },
         ],
-      },
-      {
-        // Set cache control headers for images, videos, and other assets
-        source: '/assets/:path*', // Match any asset in the public/assets directory
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable', // Cache assets for one year (31536000 seconds)
-          },
-        ],
-      },
-      {
-        // Set cache control headers for CSS files
-        source: '/assets/css/:path*', // Match any CSS file in the public/assets/css directory
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable', // Cache CSS files for one year (31536000 seconds)
-          },
-        ],
-      },
-      {
-        // Set cache control headers for CSS files
-        source: '/_next/static/css/:path*', // Match any CSS file in the _next/static/css directory
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable', // Cache CSS files for one year (31536000 seconds)
-          },
-        ],
-      },
-      {
-        // Set cache control headers for images, videos, and other assets
-        source: '/_next/static/:path*', // Match any asset in the _next/static directory
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable', // Cache assets for one year (31536000 seconds)
-          },
-        ],
-      },
+      }
     ];
   },
 };
